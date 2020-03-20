97fe79412411badc09675fab2d29b654b7dade31
Irina Zelenko <irina.zelenko@bdtech.by>
2020-03-20 15:13:32 +0300

    Add reducer

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { configureStore } from './store'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById('root')
+
+renderApp()
+
+function renderApp() {
+    const store = configureStore()
+
+    render(
+        <Provider store={store}>
+            <App />
+        </Provider>,
+        rootElement,
+    )
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/store/index.js
@@ -0,0 +1,9 @@
+import { createStore } from 'redux'
+import rootReducer from '../reducer'
+
+export function configureStore(initialState) {
+    return createStore(
+        rootReducer,
+        initialState,
+    )
+}
