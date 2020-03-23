b88eb55f9bcfdcfa0a313beb73e16b1c0246cb72
Irina Zelenko <irina.zelenko@bdtech.by>
2020-03-23 14:09:48 +0300

    Fix webpack config for pcss

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,6 @@ module.exports = {
             options: {
               importLoaders: 1,
               modules: true,
-              localIdentName: '[path]___[name]__[local]___[hash:base64:5]',
             },
           },
           { loader: 'postcss-loader' }
